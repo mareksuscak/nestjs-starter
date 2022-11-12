8970e0c43a7bd79d88702ed3d89f453c2be0e367
mareksuscak <marek.suscak@gmail.com>
2022-11-12 17:05:56 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,9 +17,13 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'array-bracket-newline': ['error', 'consistent'],
+    'array-element-newline': ['warn', 'consistent'],
+    '@typescript-eslint/interface-name-prefix': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-inferrable-types': ['off'],
+    '@typescript-eslint/no-unused-vars': ["warn", { "argsIgnorePattern": "^_" }]
   },
 };
