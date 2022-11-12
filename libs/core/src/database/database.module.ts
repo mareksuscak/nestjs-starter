12355018f493269c78c79915230a0054edc3e177
mareksuscak <marek.suscak@gmail.com>
2022-11-12 16:47:05 +0100

    Allow global context in the test environment

--- libs/core/src/database/database.module.ts
+++ libs/core/src/database/database.module.ts
@@ -3,6 +3,7 @@ import mikroOrmConfig from './mikro-orm.config';
 
 export const DatabaseModule = MikroOrmModule.forRoot({
   ...mikroOrmConfig,
+  allowGlobalContext: process.env.NODE_ENV === 'test',
   entities: [],
   entitiesTs: [],
   autoLoadEntities: true,
