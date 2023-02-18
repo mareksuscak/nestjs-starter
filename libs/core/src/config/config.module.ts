e2aa157dd3a48987f47dc3421cc2d14e4cff6ae7
mareksuscak <marek.suscak@gmail.com>
2023-02-18 15:47:47 +0100

    Global config module

--- libs/core/src/config/config.module.ts
+++ libs/core/src/config/config.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
 const currentEnvironment = process.env.NODE_ENV || 'development';
 
 export const ConfigModule = NestConfigModule.forRoot({
+  isGlobal: true,
+
   // If a variable is found in multiple files, the former one takes precedence.
   envFilePath: [`.env.${currentEnvironment}.local`, `.env.${currentEnvironment}`, '.env.local', '.env'],
 });
