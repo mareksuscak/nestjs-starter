8e983d4bcd8ff4bf3260f482d18e630d45433e81
mareksuscak <marek.suscak@gmail.com>
2023-05-12 14:07:18 +0200

    More improvements

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Placeholders for types that need to be fixed.
+ * Idea taken from [Airbnb](https://medium.com/airbnb-engineering/ts-migrate-a-tool-for-migrating-to-typescript-at-scale-cd23bfeb5cc).
+ */
+type $TSFixMe = any;
+type $TSFixMeFunction = (...args: any[]) => any;
